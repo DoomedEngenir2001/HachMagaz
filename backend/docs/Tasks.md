fedb1eaceb219c09b8d62a235bdf62bcfddb38f0
MarlouOne <majorstol@gmail.com>
2025-03-30 14:33:03 +0300

    Обновить документацию задач и изменить конфигурацию ORM для поддержки новых маршрутов и сущностей заказов и транзакций.

--- backend/docs/Tasks.md
+++ backend/docs/Tasks.md
@@ -1,7 +1,18 @@
 Что нужно сделать:
 
-* [ ]  Route выдачи товаров
-* [ ]  выдачу еды сделайте на вот этот рут /getProductCards
+* [X]  Route выдачи товаров
+* [X]  выдачу еды сделайте на вот этот рут **/getProductCards**
 
-  > Формат ответа такой {{"product": "Самса", "price":  120}, {"product": "Шава", "price":  250}}
-  >
+> Формат ответа такой {{"product": "Самса", "price":  120}, {"product": "Шава", "price":  250}}
+
+* [ ]  Механизм обработки карзины заказов **/newOrder**
+
+> {
+> "Name" : String,
+> "Surname" : String,
+> "Adress" : String
+> "Phone" : String,
+> "Cost" : Number,
+> "Status" : String,
+> "Products" : Array<Int>
+> }
--- backend/orm_models/orm_configuration.py
+++ backend/orm_models/orm_configuration.py
@@ -5,19 +5,32 @@ class ORM_Configuration:
     t_images        : str = 'images'
     t_users         : str = 'users'
     t_orders        : str = 'orders'
-    t_detailes      : str = 'detailes'
-    t_basket        : str = 'basket'
+    t_transactions  : str = 'transactions'
 
     # Классы ORM
     c_Products     : str = "Products"
     c_ProductCards : str = "ProductCards"
     c_Images       : str = "Images"
+    c_Users        : str = "Users"
+    c_Orders       : str = "Orders"
+    c_Transactions : str = "Transactions"
 
     # Связи
+    # Products <-> ProductCards
     rel_product_to_productsCards  : str = 'product'
     rel_productsCards_to_product  : str = 'productCards'
+    # ProductCards <-> Images
     rel_image_to_productsCard     : str = 'image'
     rel_productsCard_to_image     : str = 'productCards'
+    # Users <-> Orders
+    rel_user_to_orders            : str = 'user'
+    rel_orders_to_user            : str = 'orders'
+    # Orders <-> Transactions
+    rel_orders_to_transactions    : str = 'order'
+    rel_transactions_to_orders    : str = 'transactions'
+    # ProductCards <-> Transactions
+    rel_productsCards_to_transactions : str = 'productCard'
+    rel_transactions_to_productsCards : str = 'transactions'
     
     # Константы для ORM
     str_None        : str = 'None'
