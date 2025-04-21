649715245dcb055476acdd23328631c717ed294a
Zubr <kayzerms2001@gmail.com>
2025-04-21 23:49:30 +0300

    -lk-90%

--- frontend/src/pages/PersonalCabinet.vue
+++ frontend/src/pages/PersonalCabinet.vue
@@ -8,22 +8,22 @@
         <div class="flex flex-row h-[40px]">
             <div class="w-[100px] h-[40px] text-xl font-bold leading-[40px]">Имя</div>
             <div class="w-[285px] h-[40px] rounded-md border mr-0 ml-auto"><input  v-model="name" :placeholder="name" class="w-[285px] h-[40px]"></input></div>
-            <div class="w-[40px] h-[40px] bg-[url('../assets/pencil.png')]"></div>
+            <img @click="saveName" src="../assets/pencil.png" class="w-[30px] h-[30px] ml-[10px]"/>
         </div>
         <div class="flex flex-row h-[40px] mt-[40px]">
             <div class="w-[100px] h-[40px] text-xl font-bold leading-[40px]">Фамилия</div>
             <div class="w-[285px] h-[40px] rounded-md border mr-0 ml-auto"><input  v-model="surname" :placeholder="surname"  class="w-[285px] h-[40px]"></input></div>
-            <div class="w-[40px] h-[40px] bg-[url('../assets/pencil.png')]"></div>
+            <img @click="saveSurname" src="../assets/pencil.png" class="w-[30px] h-[30px] ml-[10px]"/>
         </div>
         <div class="flex flex-row h-[40px] mt-[40px]">
             <div class="w-[100px] h-[40px] text-xl font-bold leading-[40px]">Адрес</div>
             <div class="w-[285px] h-[40px] rounded-md border mr-0 ml-auto"><input  v-model="address" :placeholder="address" class="w-[285px] h-[40px]"></input></div>
-            <div class="w-[40px] h-[40px] bg-[url('../assets/pencil.png')]"></div>
+            <img @click="saveAdress" src="../assets/pencil.png" class="w-[30px] h-[30px] ml-[10px]"/>
         </div>
         <div class="flex flex-row h-[40px] mt-[40px]">
             <div class="w-[100px] h-[40px] text-xl font-bold leading-[40px]">Телефон</div>
             <div class="w-[285px] h-[40px] rounded-md border mr-0 ml-auto"><input  v-model="phone" :placeholder="phone" class="w-[285px] h-[40px]"></input></div>
-            <div class="w-[40px] h-[40px] bg-[url('../assets/pencil.png')]"></div>
+            <img @click="savePhone" src="../assets/pencil.png" class="w-[30px] h-[30px] ml-[10px]"/>
         </div>
     </div>
     <div class="flex flex-col w-full h-[60px] text-3xl flex  shadow-md">
@@ -46,11 +46,16 @@ export default{
         const productMap = store.getters.getProductFromOrders;
         console.log(productMap);
         const surname = ref(`${store.getters.getSurname}`);
+        const saveSurname = () => {store.commit('setSurname', surname.value )};
         const name = ref(`${store.getters.getName}`);
+        const saveName = () => {store.commit('setName', name.value )};
         const address = ref(`${store.getters.getAddress}`);
+        const saveAdress = () => {store.commit('setAddress', address.value )};
         const phone = ref(`${store.getters.getPhone}`);
+        const savePhone = () => {store.commit('setPhone', phone.value )};
        return {
-            surname, name, address, phone, orders, productMap
+            surname, name, address, phone, orders, productMap, saveName, saveSurname,
+            saveAdress,  savePhone
         }
     },
     components: {
