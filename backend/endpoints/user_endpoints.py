1b2788d5a3601c8e55ff415db3d90fe28edff321
Zubr <kayzerms2001@gmail.com>
2025-05-05 14:13:35 +0300

    -- / registration done

--- backend/endpoints/user_endpoints.py
+++ backend/endpoints/user_endpoints.py
@@ -24,7 +24,7 @@ from ORM_dict import ORM_dict
 from pydantic import BaseModel
 #-------------------------------------------------------------#
 from logic.l_errors   import UniqueDataError, AuthenticationError
-from logic.l_users    import create_user_row, create_user_row_reg
+from logic.l_users    import  create_user_row_reg
 from logic.l_jwt      import verify_jwt, create_jwt
 #-------------------------------------------------------------#
 class sign_up_request(BaseModel):
@@ -33,40 +33,27 @@ class sign_up_request(BaseModel):
     email: str
     phone: str
 
+class sign_up_response(BaseModel):
+    token: str
+    
+class login_req(BaseModel):
+    login: str
+    password: str
+
 user_routes = APIRouter()
 
 
 @user_routes.post("/registration")
-async def create_user(   req: sign_up_request, response_model=None)-> Any: # ДОДЕЛАТЬ
+async def create_user(   req: sign_up_request)->dict: # ДОДЕЛАТЬ
     _result = await create_user_row_reg( login=req.login, password=req.password, 
-                                    email=req.email, phone=req.phone )
+                                    email=req.email, phone=req.phone)
     if isinstance(_result, UniqueDataError):
         return {"message": _result.show()}
     elif isinstance(_result, str):
-        return {"token": await create_jwt( _result)}
-
-@user_routes.get("/is_user_exist")
-async def is_user_exist(login: str = None, email: str = None, phone: str = None):
-    _result : bool = False
-    if login is not None:
-        _result = _result or await Users.is_login_exist(login)
-    if email is not None:
-        _result = _result or await Users.is_email_exist(email)
-    if phone is not None:
-        _result = _result or await Users.is_phone_exist(phone)
+        token = await create_jwt( _result)
+        return {"token":token}
     
-    return {"message": _result}
-
-@user_routes.get("/get_user")
-async def get_user(user_id: int):
-    user : Users = await Users.get_rowById(cls=Users, id=user_id)
-    if isinstance(user, Users):
-        return user.toDict()
-    else:
-        return {"message": "User not found"}
-
-# TODO: Что то не так с этой функцией
-@user_routes.post("/login_user")
+@user_routes.post("/login")
 async def login_user(password: str = None, login: str = None
                      #, email: str = None, phone: str = None
                      ):
@@ -88,3 +75,25 @@ async def login_user(password: str = None, login: str = None
             return {"token": token}
         else:
             return AuthenticationError("Invalid password").show
+        
+@user_routes.get("/is_user_exist")
+async def is_user_exist(login: str = None, email: str = None, phone: str = None):
+    _result : bool = False
+    if login is not None:
+        _result = _result or await Users.is_login_exist(login)
+    if email is not None:
+        _result = _result or await Users.is_email_exist(email)
+    if phone is not None:
+        _result = _result or await Users.is_phone_exist(phone)
+    
+    return {"message": _result}
+
+@user_routes.get("/get_user")
+async def get_user(user_id: int):
+    user : Users = await Users.get_rowById(cls=Users, id=user_id)
+    if isinstance(user, Users):
+        return user.toDict()
+    else:
+        return {"message": "User not found"}
+
+# TODO: Что то не так с этой функцией
