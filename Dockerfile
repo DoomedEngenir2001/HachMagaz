82076610543b3bdb4302c4fa79fdb45634430710
DadyshkaGoga <95309197+DadyshkaGoga@users.noreply.github.com>
2025-05-16 00:58:40 +0300

    [PersonalCabinet] inputs need logic from backend + fix typing

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ WORKDIR /app
 COPY backend ./
 EXPOSE 8000
 RUN pip install --no-cache-dir -r requirements.txt
-# RUN python db_modules/tables_creator.py
-# RUN python orm_models/test_data_loader.py
+RUN python db_modules/tables_creator.py
+RUN python orm_models/test_data_loader.py
 ENTRYPOINT ["uvicorn", "fast_main:app", "--host", "0.0.0.0", "--reload"]
--- frontend/src/pages/PersonalCabinet.vue
+++ frontend/src/pages/PersonalCabinet.vue
@@ -1,66 +1,62 @@
-<template>
-<div class="flex flex-col">
-    <div class="flex flex-row w-full h-[60px] text-3xl flex shadow-xl">
-        <div class="text-3xl font-bold pl-[20px] leading-[60px]">Личный кабинет</div>
-        <orangeBtnTS @click="$router.push('/')" class="mr-[50px] ml-auto">Главная</orangeBtnTS>
-    </div>
-    <div class="flex flex-col w-2/5 mt-[40px] ml-[20px]">
-        <div class="flex flex-row h-[40px]">
-            <div class="w-[100px] h-[40px] text-xl font-bold leading-[40px]">Имя</div>
-            <div class="w-[285px] h-[40px] rounded-md border mr-0 ml-auto"><input  v-model="name" :placeholder="name" class="w-[285px] h-[40px]"></input></div>
-            <img @click="saveName" src="../assets/pencil.png" class="w-[30px] h-[30px] ml-[10px]"/>
-        </div>
-        <div class="flex flex-row h-[40px] mt-[40px]">
-            <div class="w-[100px] h-[40px] text-xl font-bold leading-[40px]">Фамилия</div>
-            <div class="w-[285px] h-[40px] rounded-md border mr-0 ml-auto"><input  v-model="surname" :placeholder="surname"  class="w-[285px] h-[40px]"></input></div>
-            <img @click="saveSurname" src="../assets/pencil.png" class="w-[30px] h-[30px] ml-[10px]"/>
-        </div>
-        <div class="flex flex-row h-[40px] mt-[40px]">
-            <div class="w-[100px] h-[40px] text-xl font-bold leading-[40px]">Адрес</div>
-            <div class="w-[285px] h-[40px] rounded-md border mr-0 ml-auto"><input  v-model="address" :placeholder="address" class="w-[285px] h-[40px]"></input></div>
-            <img @click="saveAdress" src="../assets/pencil.png" class="w-[30px] h-[30px] ml-[10px]"/>
-        </div>
-        <div class="flex flex-row h-[40px] mt-[40px]">
-            <div class="w-[100px] h-[40px] text-xl font-bold leading-[40px]">Телефон</div>
-            <div class="w-[285px] h-[40px] rounded-md border mr-0 ml-auto"><input  v-model="phone" :placeholder="phone" class="w-[285px] h-[40px]"></input></div>
-            <img @click="savePhone" src="../assets/pencil.png" class="w-[30px] h-[30px] ml-[10px]"/>
-        </div>
-    </div>
-    <div class="flex flex-col w-full h-[60px] text-3xl flex  shadow-md">
-        <div class="text-3xl font-bold pl-[20px] leading-[60px]">Мои заказы</div>
-        <orderContainer v-for="order in orders" :date="order.createTime" 
-        :Address="order.address" :cost="order.cost" :products="productMap.get(order.id)"
-        class="mt-[5px]"></orderContainer>
-    </div>
-</div>
-</template>
-<script lang="ts">
+<script setup ="ts">
 import orangeBtnTS from '../components/orangeBtnTS.vue';
-import {  ref, onBeforeMount } from 'vue';
+import {  ref, onBeforeMount, onMounted } from 'vue';
 import { useStore } from 'vuex';
-import orderContainer from '../components/orderContainer.vue';
-export default{
-    setup(){
-        const store = useStore();
-        const orders = store.getters.getOrders; 
-        const productMap = store.getters.getProductFromOrders;
-        console.log(productMap);
-        const surname = ref(`${store.getters.getSurname}`);
-        const saveSurname = () => {store.commit('setSurname', surname.value )};
-        const name = ref(`${store.getters.getName}`);
-        const saveName = () => {store.commit('setName', name.value )};
-        const address = ref(`${store.getters.getAddress}`);
-        const saveAdress = () => {store.commit('setAddress', address.value )};
-        const phone = ref(`${store.getters.getPhone}`);
-        const savePhone = () => {store.commit('setPhone', phone.value )};
-       return {
-            surname, name, address, phone, orders, productMap, saveName, saveSurname,
-            saveAdress,  savePhone
-        }
-    },
-    components: {
-        orangeBtnTS,
-        orderContainer
-    }
-}
+// import orderContainer from '../components/orderContainer.vue';
+import UserInput from '../components/PersonalCabinet/MainBlock/UserInput.vue'
+
+const personalCabinetItems = ref({
+    inputs: []
+});
+
+onMounted(() => {
+    personalCabinetItems.value = {
+        inputs: [
+            {
+                name: 'Имя',
+                value: 'Имя Абобус',
+            },
+            {
+                name: 'Фамилия',
+                value: 'Фамилия Абобус',
+            },
+            {
+                name: 'Адрес',
+                value: 'Адрес Абобус',
+            },
+            {
+                name: 'Телефон',
+                value: 'Телефон Абобус',
+            },
+        ]
+    };
+});
 </script>
+
+
+<template>
+    <div class="flex flex-col">
+        <!-- TODO Как будто бы можно более глобально вынести не таскать за собой header -->
+        <div class="flex flex-row w-full h-[60px] text-3xl shadow-xl">
+            <div class="text-3xl font-bold pl-[20px] leading-[60px]">Личный кабинет</div>
+            <orangeBtnTS @click="$router.push('/')" class="mr-[50px] ml-auto">Главная</orangeBtnTS>
+        </div>
+
+        <div class="m-5">
+            <div class="flex flex-col gap-5 pt-5">
+                <UserInput
+                v-for="item in personalCabinetItems.inputs" :key="item"
+                :name="item.name"
+                :value="item.value"
+                />
+            </div>
+        </div>
+        <div class="flex flex-col w-full h-[60px] text-3xl shadow-md">
+            <div class="text-3xl font-bold pl-[20px] leading-[60px]">Мои заказы</div>
+            <orderContainer v-for="order in orders" :key="order" :date="order.createTime"
+            :Address="order.address" :cost="order.cost" :products="productMap.get(order.id)"
+            class="mt-[5px]"></orderContainer>
+        </div>
+    </div>
+
+</template>
