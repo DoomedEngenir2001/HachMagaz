9489e995608db892afabcccac63ed2fb0d7d4427
Zubr <kayzerms2001@gmail.com>
2025-06-12 20:06:53 +0300

    -- secure roots front

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -5,13 +5,30 @@ import MainPage from './pages/mainPage.vue';
 import orderPage from './pages/orderPage.vue';
 import PersonalCabinet from './pages/PersonalCabinet.vue';
 import adminPage from './pages/adminPage.vue'
+import {getStorage} from "./plugins/persistent"
 const routes = [
     {path: '/', component: MainPage},
-    {path: '/orderMap', component: OrderMapPage},
-    {path: '/order', component: orderPage},
-    {path: '/personalCabinet', component: PersonalCabinet},
-    // {path: '/adminPage', component: adminPage},
+    {path: '/orderMap', component: OrderMapPage, meta:{
+        requiresAuth: true
+    }},
+    {path: '/order', component: orderPage, meta:{
+        requiresAuth: true
+    }},
+    {path: '/personalCabinet', component: PersonalCabinet, meta:{
+        requiresAuth: true
+    }}
 ];
-
 const router = createRouter({routes, history: createWebHistory()});
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth){
+        const token = localStorage.getStorage("token");
+        if (token != ''){
+            next()
+        }else{
+            next('/');
+        }
+    } else{
+        next();
+    }
+})
 export default  router;
